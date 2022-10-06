dd5d41f4b4a18a73c1ee038c8bc2a7f5d23e1b12
jsalim-neu <78458754+jsalim-neu@users.noreply.github.com>
2022-10-06 16:50:06 -0400

    assignment complete

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let newestPointId = 1;
-
 //plotting a point from its coordinate values
 function plotPoint(x, y) {
 	let svg = document.getElementById("plot"); //retrieving the SVG plot
@@ -7,19 +5,20 @@ function plotPoint(x, y) {
 		"<circle cx=" + (x * 25) // scaling x-value of point
 		+ " cy=" + (250 - y * 25) // scaling y-value of point; note that on a scatterplot, points ascend as their y-values increase
 		+ " r=10 class =\"point\"" 
-		+ "id=" + newestPointId + ">";
+		+ "id=(" + x + "," + y + ")>";
 	svg.innerHTML = svg.innerHTML + "\n" + circleHTML; // adding the circle to the HTML SVG 
+	let points = document.querySelectorAll(".point");
+	points.forEach(point => {
+		point.addEventListener('click', (e) => {
+			pointClicked(point.id)});
+	});
 	console.log("Plotted point: " + x + ", " + y); // delete later
-	currentPoint = document.getElementById(newestPointId.toString());
-	currentPoint.addEventListener('click', pointClicked);
-	newestPointId = newestPointId + 1;
 }
 
 plotPoint(1, 2);
 plotPoint(2, 4);
 plotPoint(6, 2);
 plotPoint(9, 9);
-
 
 
 
@@ -31,6 +30,17 @@ function userAddPoint() {
 	plotPoint(x, y);
 }
 
-function pointClicked() {
+function pointClicked(pointId) {
 	console.log("point clicked!");
+	let point = document.getElementById(pointId);
+	let pointDisplay = document.getElementById("lastClicked");
+	pointDisplay.innerHTML = "Last clicked point: " + point.id;
+	pointDisplay.style.display = "block";
+	if (point.style.strokeWidth == 0 || point.style.strokeWidth == "0px") {
+		point.style.strokeWidth = "2px";
+
+	} else {
+		point.style.strokeWidth = "0px";
+	}
+	console.log(point.style.strokeWidth);
 }
