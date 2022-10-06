f57d5cd37840010427ec3923980850d7f3c1f1e2
jsalim-neu <78458754+jsalim-neu@users.noreply.github.com>
2022-10-06 17:03:50 -0400

    commented out, assignment complete

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ function plotPoint(x, y) {
 	let points = document.querySelectorAll(".point");
 	points.forEach(point => {
 		point.addEventListener('click', (e) => {
-			pointClicked(point.id)});
+			pointClicked(point.id)}); //adding an event listener that runs a handler; note that the handler takes in the clicked point's ID
 	});
-	console.log("Plotted point: " + x + ", " + y); // delete later
 }
 
+//plotting assignment-specified points
 plotPoint(1, 2);
 plotPoint(2, 4);
 plotPoint(6, 2);
 plotPoint(9, 9);
 
 
-
+//plotting user-added points
 document.getElementById("addPointButton").addEventListener('click', userAddPoint);
 
 function userAddPoint() {
@@ -30,17 +30,17 @@ function userAddPoint() {
 	plotPoint(x, y);
 }
 
+//handling for clicking a point on the SVG
 function pointClicked(pointId) {
-	console.log("point clicked!");
-	let point = document.getElementById(pointId);
-	let pointDisplay = document.getElementById("lastClicked");
-	pointDisplay.innerHTML = "Last clicked point: " + point.id;
-	pointDisplay.style.display = "block";
+	let point = document.getElementById(pointId); //selecting clicked point
+	let pointDisplay = document.getElementById("lastClicked"); //selecting div that will display the last clicked point coordinates
+	pointDisplay.innerHTML = "Last clicked point: " + point.id; //injecting HTML with clicked point text
+	pointDisplay.style.display = "block"; //show div
+	//logic for showing/hiding point borders
 	if (point.style.strokeWidth == 0 || point.style.strokeWidth == "0px") {
 		point.style.strokeWidth = "2px";
 
 	} else {
 		point.style.strokeWidth = "0px";
 	}
-	console.log(point.style.strokeWidth);
 }
